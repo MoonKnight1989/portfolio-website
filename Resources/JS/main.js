45a0e2aec06a963a14dacb08093d3fb768d96394
Andy Masson <andymasson8@hotmail.co.uk>
2023-01-12 18:45:57 +0000

    fixed JS script on hero button

--- Resources/JS/main.js
+++ Resources/JS/main.js
@@ -2,8 +2,14 @@
 let button = document.getElementById('mainButton');
 
 function changeText() {
-    button.innerHTML = "yes click here!";
+    button.innerHTML = "Yes Click Here!";
     button.style.backgroundColor = 'blue';
-}
+};
 
-button.addEventListener('click', changeText);
+function changeTextBack() {
+    button.innerHTML = "View my work";
+    button.style.backgroundColor = "#FF1577"
+};
+
+button.addEventListener('mouseover', changeText);
+button.addEventListener('mouseout', changeTextBack);
